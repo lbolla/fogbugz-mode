6ca657fa161531beb9d0ee834604d87934784478
Rudolf Olah <rolah@trapeze.com>
2013-01-07 15:00:32 -0500

    Updated the properties that are inserted when creating a new case. Added variable ORG-FOGBUGZ-DONE-STATUS-IDS.

--- org-fogbugz.el
+++ org-fogbugz.el
@@ -3,15 +3,35 @@
 (require 'fogbugz)
 (require 'org)
 
+(defvar org-fogbugz-done-status-ids
+  '()
+  "List of status ids that indicate a DONE status. You can get a
+list of statuses using `fogbugz-list-statuses'.")
+
 (defun org-insert-todo-fogbugz (case-id)
   "Insert a new heading that uses information from a FogBugz case
 with a `case-id'."
   (let ((buf (current-buffer))
-        (case (first (fogbugz-search-cases (number-to-string case-id) '(id title)))))
+        (fb-case (first (fogbugz-search-cases (number-to-string case-id) '(id title opener-id area-name category-name status-id status-name original-estimate due-date)))))
     (set-buffer buf)
     (org-insert-todo-heading-respect-content)
-    (insert (rest (assoc 'title case)))
-    (org-entry-put (point) "CaseId" (number-to-string (rest (assoc 'id case))))))
+    (insert (rest (assoc 'title fb-case)))
+    (flet ((org-put (property column &optional transformer)
+                    (org-entry-put (point) property (if transformer
+                                                        (funcall transformer (rest (assoc column fb-case)))
+                                                      (rest (assoc column fb-case))))))
+      (org-put "CaseId" 'id 'number-to-string)
+      (org-put "StatusId" 'status-id 'number-to-string)
+      (org-put "Status" 'status-name)
+      (org-put "OriginalTitle" 'title)
+      (org-put "OpenedBy" 'opener-id 'number-to-string)
+      (org-put "Area" 'area-name)
+      (org-put "Category" 'category-name)
+      (org-put "Effort" 'original-estimate)
+      (org-put "LastUpdated" 'last-updated-on (lambda (date) (if date (first (split-string date "T" t)) ""))))
+    (if (rest (assoc 'due-date fb-case))
+        (org-deadline nil (rest (assoc 'due-date fb-case))))
+))
 
 (defun org-fogbugz-tag-alist ()
   "Returns an alist that can be used with `org-tag-alist` or
